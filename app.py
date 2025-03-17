b273d894fa280fb6973aa9ef23f294d3160fdc05
ThomasShelbee <defolt.pon.da@gmail.com>
2025-03-17 18:07:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,4 @@
-from flask import Flask, request, jsonify, send_file
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_cors import CORS
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cd_collection.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-CORS(app)
-
-# модель для хранения задачи
-class Items(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(15), nullable=False)
-    title = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    s_size = db.Column(db.Integer, nullable=False)
-    m_size = db.Column(db.Integer, nullable=False)
-    l_size = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-
-class Promos(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(15), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-
-
-with app.app_context():
-    db.create_all()
+from models import *
 
 def present_item(item):
     return {

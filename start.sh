9607f45410cd475ab13013d312b35c7c0b5dd785
ThomasShelbee <general.mayorov@gmail.com>
2025-05-26 22:10:19 +0300

    Initial commit

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 docker compose up --build -d
 sudo docker cp kk:/app/frontend/dist .
-sudo chown -R www-data:www-data dist
-sudo chmod -R 777 dist
+sudo chown -R nikita:nikita dist
+sudo usermod -a -G nikita www-data

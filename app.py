b9304d6c9d0ad235f9a8cb07066db5edfd6542d4
ThomasShelbee <general.mayorov@gmail.com>
2025-05-28 17:15:41 +0300

    Initial commit

--- app.py
+++ app.py
@@ -4,20 +4,14 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_mail import Mail, Message
 
+from config import *
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cd_collection.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 CORS(app)
-
-app.config['MAIL_SERVER'] = 'postbox.cloud.yandex.net'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'YCAJE6Zi1vyP-G5E9py5UJmtW'
-app.config['MAIL_PASSWORD'] = 'BFTJIMReImC3SdhmYyEw+wpkIYD63IGkBWf4nh8jE5Du'
-app.config['MAIL_USE_TLS'] = True
-mail = Mail(app)
-
 
 # модель для хранения задачи
 class Items(db.Model):
---
+++ config.py
@@ -0,0 +1,6 @@
+app.config['MAIL_SERVER'] = 'postbox.cloud.yandex.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'YCAJE6Zi1vyP-G5E9py5UJmtW'
+app.config['MAIL_PASSWORD'] = 'BFTJIMReImC3SdhmYyEw+wpkIYD63IGkBWf4nh8jE5Du'
+app.config['MAIL_USE_TLS'] = True
+mail = Mail(app)
